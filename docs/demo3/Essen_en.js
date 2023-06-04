44da30afaea716f93e19fac99dd23ea9cfaa7a85
Jens Grätzer <44551846+jensgraetzer@users.noreply.github.com>
2023-06-04 14:47:52 +0200

    Initial commit

---
+++ docs/demo3/Essen_en.js
@@ -0,0 +1,16 @@
+var mediafile = "Essen_LD.mp4";
+//var lan_1 = "english";
+//var subt_1 = [
+var subt = [
+ [0.010,3.503,"Essen is the central and<br>second largest city of the Ruhr,"],
+ [3.503,5.498,"the largest urban area in Germany."],
+ [5.498,9.750,"Its population of 583,109"],
+ [9.750,11.994,"makes it the ninth largest city of Germany,"],
+ [11.994,15.959,"as well as the fourth largest city<br>of the federal state of North Rhine-Westphalia."],
+ [15.959,20.153,"On the Ruhr and Emscher rivers,<br>Essen geographically is part of the Rhineland"],
+ [20.153,22.394,"and the larger Rhine-Ruhr Metropolitan Region."],
+ [22.394,25.500,"Essen is seat to several of the region's authorities,"],
+ [25.500,30.266,"as well as to eight of the 100 largest<br>publicly held German corporations by revenue,"],
+ [30.266,32.300,"including two DAX corporations."],
+ [32.300,32.900,""]
+];
